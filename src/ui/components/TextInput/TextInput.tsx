0d308d9768c02e6af3b68537973ea2a2af380cf9
yaroslav <20459871+dusaniuk@users.noreply.github.com>
2024-08-05 13:47:42 +0300

    feat: add custom eye icon for password TextInput

--- src/ui/components/TextInput/TextInput.tsx
+++ src/ui/components/TextInput/TextInput.tsx
@@ -1,6 +1,8 @@
+import { EyeClosedIcon } from "@ui/icons/EyeClosedIcon";
+import { EyeIcon } from "@ui/icons/EyeIcon";
 import { cva, type VariantProps } from "class-variance-authority";
 import classNames from "classnames";
-import { InputHTMLAttributes } from "react";
+import { InputHTMLAttributes, useState } from "react";
 
 const textInputVariants = cva(
   "border border-solid rounded-[10px] py-3 px-5 max-h-12 focus:outline-none transition-colors",
@@ -31,9 +33,38 @@ export type TextInputProps = TextInputVariantProps &
   InputHTMLAttributes<HTMLInputElement>;
 
 export function TextInput(props: TextInputProps) {
-  const { error = false, valid = false, ...otherProps } = props;
+  const {
+    type: inputType,
+    error = false,
+    valid = false,
+    ...otherProps
+  } = props;
+
+  const [showPassword, setShowPassword] = useState(false);
 
   const classes = classNames(textInputVariants({ error, valid }));
 
-  return <input className={classes} {...otherProps} />;
+  const IconComponent = showPassword ? EyeIcon : EyeClosedIcon;
+
+  function handleTogglePasswordVisibility() {
+    setShowPassword((prev) => !prev);
+  }
+
+  return (
+    <div className="flex flex-col relative">
+      <input
+        type={showPassword ? "text" : inputType}
+        className={classes}
+        {...otherProps}
+      />
+      {inputType === "password" && (
+        <div className="absolute right-5 top-1/2 transform -translate-y-1/2 cursor-pointer text-gray-400">
+          <IconComponent
+            className="w-6 h-6"
+            onClick={handleTogglePasswordVisibility}
+          />
+        </div>
+      )}
+    </div>
+  );
 }
